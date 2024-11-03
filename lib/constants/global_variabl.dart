1fbce5e33600db4ef288250c07393e785dceeb9c
billamama4u <153830240+billamama4u@users.noreply.github.com>
2024-11-04 00:15:05 +0530

    Update global_variabl.dart

--- lib/constants/global_variabl.dart
+++ lib/constants/global_variabl.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
-String uri = kReleaseMode
-    ? 'https://exampla-12ac6.web.app' // Production URL
-    : 'http://10.0.2.2:3000';
+
+String uri = 'http://10.0.2.2:3000';
 
 class GlobalVariable {
   // COLORS
