ebc11419ff7e07ca5ae9d1a0138df9ca61bd7687
billamama4u <153830240+billamama4u@users.noreply.github.com>
2024-11-04 00:18:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-// import package
+// Import packages
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose'); // Correct the spelling here
-const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
+const PORT = process.env.PORT || 3000;
 const url = "mongodb+srv://trai7078:manu3883@billa.duy1bsd.mongodb.net/?retryWrites=true&w=majority&appName=billa";
 
 const app = express();
 
-// other imports
+// Import routes
 const authRouter = require('./lib/routes/auth.js');
 const adminRouter = require('./lib/routes/admin.js');
 const productRouter = require('./lib/routes/product.js');
 const userRouter = require('./lib/routes/user.js');
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(authRouter);
 app.use(productRouter);
 app.use(adminRouter);
 app.use(userRouter);
-// connecting to MongoDB
+
+// Connect to MongoDB
 async function connectDB() {
     try {
         await mongoose.connect(url);
-        console.log("Connection successful");
+        console.log("Database connection successful");
     } catch (e) {
         console.error("Error connecting to the database:", e);
     }
@@ -32,14 +33,12 @@ async function connectDB() {
 
 connectDB();
 
+// Example route
 app.get('/hello-world', (req, res) => {
-    res.send('hello world');
+    res.send('Hello world');
 });
 
-app.listen(PORT,"0.0.0.0", () => {
-    console.log(`Connected to port ${PORT}`);
-});
-
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
